8fb5cc9e8c0d385764cb870d2c75a997e73491ed
HsuenJuLi <lh95024@gmail.com>
2020-10-09 14:46:11 +0800

    Create Groups Folder to clear messy files

---
+++ GITFollewer/Custom Views/GFAlertContainerView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAlertContainerView.swift
+//  GITFollewer
+//
+//  Created by Hsuen-Ju Li on 2020/10/9.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
