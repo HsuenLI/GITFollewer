c5659a87209936a705a338f85af72cc5940338d8
HsuenJuLi <lh95024@gmail.com>
2020-10-09 15:24:36 +0800

    Create User and Follower Model

---
+++ GITFollewer/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GITFollewer
+//
+//  Created by Hsuen-Ju Li on 2020/10/9.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
