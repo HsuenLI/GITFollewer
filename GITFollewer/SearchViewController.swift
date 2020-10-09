d6cae13e909c8aedafb28e63a8035e2bfdf7b08d
HsuenJuLi <lh95024@gmail.com>
2020-10-09 14:16:31 +0800

    create customAlertController, and configure in SearchViewController

--- GITFollewer/SearchViewController.swift
+++ GITFollewer/SearchViewController.swift
@@ -39,6 +39,7 @@ class SearchViewController: UIViewController {
         
         guard isUsernameEntered else {
             print("No username")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username, We need to know who to look for.", buttonTitle: "Ok")
             return
         }
         let followerListVC = FollowerListViewController()
