a0bda9c45780cd331b9370aa179980365f0f47aa
HsuenJuLi <lh95024@gmail.com>
2020-10-11 01:36:38 +0800

    create loading view

--- GITFollewer/Screens/FollowerListViewController.swift
+++ GITFollewer/Screens/FollowerListViewController.swift
@@ -51,8 +51,10 @@ class FollowerListViewController: UIViewController {
 
 
     func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             
             switch result {
             case .failure(let error):
