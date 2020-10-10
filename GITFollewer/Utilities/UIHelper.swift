67f2d968e8e794b395412e393c9af127e3abe1ed
HsuenJuLi <lh95024@gmail.com>
2020-10-10 20:38:03 +0800

    Implementation UIHelper and [weak self] keyword

---
+++ GITFollewer/Utilities/UIHelper.swift
@@ -0,0 +1,25 @@
+//
+//  UIHelper.swift
+//  GITFollewer
+//
+//  Created by Hsuen-Ju Li on 2020/10/10.
+//
+
+import UIKit
+
+struct UIHelper {
+    
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let avaliableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = avaliableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = .init(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+}
