b2c1a07e08a77e70ce28b33be4ad10068264ff13
HsuenJuLi <lh95024@gmail.com>
2020-10-12 03:17:28 +0800

    Create UserInfoController, Add child controller in another controller

---
+++ GITFollewer/Utilities/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  GITFollewer
+//
+//  Created by Hsuen-Ju Li on 2020/10/12.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
