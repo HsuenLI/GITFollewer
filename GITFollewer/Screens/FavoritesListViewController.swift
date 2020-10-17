0a07d515fa80e2152e6871710b7d62faf75a4003
HsuenJuLi <lh95024@gmail.com>
2020-10-17 21:43:09 +0800

    Create GFDataLoadingVC

--- GITFollewer/Screens/FavoritesListViewController.swift
+++ GITFollewer/Screens/FavoritesListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FavoritesListViewController: UIViewController {
+class FavoritesListViewController: GFDataLoadingVC {
     
     let tableView = UITableView()
     var favorites: [Follower] = []
