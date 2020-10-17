1bea37fa510acbe4b6f5c0f702552fa71fe8e63e
HsuenJuLi <lh95024@gmail.com>
2020-10-17 12:01:55 +0800

    add dateLabel to userInfoVC

---
+++ GITFollewer/Extensions/Date+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Date+Ext.swift
+//  GITFollewer
+//
+//  Created by Hsuen-Ju Li on 2020/10/17.
+//
+
+import Foundation
+
+extension Date {
+    
+    func convertToMonthFormat() -> String {
+        let dateFomatter = DateFormatter()
+        dateFomatter.dateFormat = "MMM yyyy"
+        dateFomatter.locale = Locale(identifier: "zh_Hant_TW")
+        return dateFomatter.string(from: self)
+    }
+}
---
+++ GITFollewer/Extensions/String+Ext.swift
@@ -0,0 +1,24 @@
+//
+//  String+Ext.swift
+//  GITFollewer
+//
+//  Created by Hsuen-Ju Li on 2020/10/17.
+//
+
+import Foundation
+
+extension String {
+    
+    func convertToDate() -> Date? {
+        let dateFommatter = DateFormatter()
+        dateFommatter.dateFormat = "yyyy-MM-dd 'T HH:mm:ssZ"
+        dateFommatter.locale = Locale(identifier: "zh_Hant_TW")
+        dateFommatter.timeZone = .current
+        return dateFommatter.date(from: self)
+    }
+    
+    func convertToDisplayFormat() -> String {
+        guard let date = self.convertToDate() else { return "N/A"}
+        return date.convertToMonthFormat()
+    }
+}
