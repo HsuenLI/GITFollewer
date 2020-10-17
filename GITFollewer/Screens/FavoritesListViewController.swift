7c7357b63c2f803a640a84d561e85524b7bb3746
HsuenJuLi <lh95024@gmail.com>
2020-10-17 13:57:19 +0800

    Retrieved favorites user in favoritesVC

--- GITFollewer/Screens/FavoritesListViewController.swift
+++ GITFollewer/Screens/FavoritesListViewController.swift
@@ -12,5 +12,14 @@ class FavoritesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orange
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
 }
